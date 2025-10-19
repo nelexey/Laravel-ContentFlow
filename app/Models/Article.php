b7726636990f55b91c8b1a7e5e604770c04066a7
nelexey <codenamed.asyncz@gmail.com>
2025-10-19 15:39:41 +0300

    Модели и Миграции (Articles, Cats, Comments) Привязка комментариев к пользователю (User)

---
+++ app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
